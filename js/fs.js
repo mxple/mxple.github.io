aa87cf945e12a8964ffcffa4931acc265e3421f7
RIN <83033020+Mxple@users.noreply.github.com>
2022-07-08 21:42:27 -0400

    Added psudo filesystem

---
+++ js/fs.js
@@ -0,0 +1,16 @@
+var FS = {
+    "bin": {
+        "ls": "js code",
+        "cd": "js code",
+        "rm": "js code"
+    },
+    "dev": {},
+    "etc": {},
+    "usr": {
+        "guest": {
+            "foo.txt": "some string",
+            "bar.txt": "some other string"
+        }
+    }
+}
+window.addEventListener("click",console.log(FS));

1bde5cf3940c92252f14cd57e33ed218c9b64a0c
RIN <83033020+Mxple@users.noreply.github.com>
2023-11-20 20:06:00 -0500

    Update commands.js

--- terminal/js/commands.js
+++ terminal/js/commands.js
@@ -251,7 +251,7 @@ function neofetch(p) {
     });
     // this is actually pfetch lol
     addLine("</span style='white-space:nowrap'>\
-       .:'      "+ user + "@mxpleRIN.moe<br>\
+       .:'      "+ user + "@mxpleRIN<br>\
     _ :'_       <b>os</b>     rinOS<br>\
  .'`_`-'_``.    <b>host</b>   "+ browserName + "<br>\
 :________.-'    <b>kernel</b> 4.2.0<br>\
